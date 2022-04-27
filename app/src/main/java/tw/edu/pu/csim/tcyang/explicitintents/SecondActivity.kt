af1067dcce0fd45eeeb8d240225de0b008d696ff
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-04-27 14:42:28 +0800

    v.4 JavaScript與GoBack

--- app/src/main/java/tw/edu/pu/csim/tcyang/explicitintents/SecondActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/explicitintents/SecondActivity.kt
@@ -3,6 +3,7 @@ package tw.edu.pu.csim.tcyang.explicitintents
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
@@ -17,6 +18,12 @@ class SecondActivity : AppCompatActivity() {
         webv = findViewById(R.id.webv)
         val website:String? = intent.getStringExtra("website")
         if (website != null){
+            // 如果想讓網頁顯示在自己的WebView上
+            webv.setWebViewClient(WebViewClient())
+
+            //允許網頁使用JavaScript
+            webv.settings.javaScriptEnabled = true
+
             webv.loadUrl(website)
         }
 
@@ -24,5 +31,14 @@ class SecondActivity : AppCompatActivity() {
         btnBack.setOnClickListener({
             finish()
         })
+    }
+
+    override fun onBackPressed(){
+        if (webv.canGoBack()){
+            webv.goBack()
+        }
+        else{
+            finish()
+        }
     }
 }
