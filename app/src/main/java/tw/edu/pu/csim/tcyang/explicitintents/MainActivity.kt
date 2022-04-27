6b6a293cf333fdc12b6fb1e85afdabdbebc3be6b
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-04-27 13:52:30 +0800

    v.1 版面設計及findViewById

--- app/src/main/java/tw/edu/pu/csim/tcyang/explicitintents/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/explicitintents/MainActivity.kt
@@ -2,10 +2,20 @@ package tw.edu.pu.csim.tcyang.explicitintents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnNext: Button
+    lateinit var edt: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnNext = findViewById(R.id.btnNext)
+        edt = findViewById(R.id.edt)
+        edt.setText("https://www.pu.edu.tw")
     }
 }
